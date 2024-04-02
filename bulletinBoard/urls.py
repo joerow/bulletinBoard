175f440812693a7ff3b061a5779e3c445b65912a
joerow <joerow@gmail.com>
2024-04-02 13:06:21 +0800

    part 4 completed

--- bulletinBoard/urls.py
+++ bulletinBoard/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('', include('bulletin.urls')),
+
 ]
